a36545ffe3b302b7d5841e33b91f96a71183fb0f
David Dale <ddale@yandex-team.ru>
2018-05-06 19:22:24 +0300

    try to make the first loggable bot

---
+++ main.py
@@ -0,0 +1,20 @@
+import telebot
+import os
+from heroku_logging import DBLogger
+
+TOKEN = os.environ['TOKEN']
+bot = telebot.TeleBot(TOKEN)
+dblogger = DBLogger(os.environ['DATABASE_URL'])
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет! Я чатбот Каппа Веди. Я пока умею очень мало, но буду учиться. ")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    response = "Если вы есть – будьте первыми!"
+    dblogger.log_message(message, response)
+    bot.reply_to(message, response)
+
+bot.polling()
