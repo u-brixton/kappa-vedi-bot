4c640fa3526c163f471783af89ee8ebae9c02e90
helmeton <alfhelmeton@gmail.com>
2019-05-23 23:15:54 +0300

    obcsenity

--- main_coffee.py
+++ main_coffee.py
@@ -22,6 +22,7 @@ from flask import Flask, request
 from telebot import types
 
 from utils import matchers
+from dog_mode import doggy_style
 
 ON_HEROKU = os.environ.get('ON_HEROKU')
 TOKEN = os.environ['TOKEN']
@@ -231,7 +232,8 @@ def process_message(msg):
         try_coffee_management,
         try_membership_management,
         try_event_edition,
-        try_unauthorized_help
+        try_unauthorized_help,
+        doggy_style
     ]:
         ctx = handler(ctx, database=database)
         if ctx.intent is not None:
