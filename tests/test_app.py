5547a3c4826c478e093d9a24c771419ba56f40f7
David Dale <ddale@yandex-team.ru>
2019-08-29 22:50:14 +0300

    fix the import test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,13 @@
+import mongomock
+
+from utils.database import Database
+from unittest.mock import patch
+
+
 def test_import():
-    from main import server  # noqa
+    with patch.dict('os.environ', {'TOKEN': 'mock token', 'MONGODB_URI': 'mock uri'}):
+        def new_setup_client(obj, mongo_url):
+            obj._mongo_db = mongomock.MongoClient().db
+
+        with patch.object(Database, '_setup_client', new=new_setup_client):
+            from main import server  # noqa
